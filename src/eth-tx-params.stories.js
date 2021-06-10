47cab77c737f1274572a53b7537a7d56d09fbb14
Dan Finlay <dan@danfinlay.com>
2021-06-09 20:15:42 -0700

    Storybook added, Generated decodings

---
+++ src/eth-tx-params.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { text, boolean } from '@storybook/addon-knobs';
+import EthTxParams from '.';
+
+export default {
+  title: 'Eth Tx Params',
+};
+
+export const primaryType = () => {
+  const decoding = {};
+  
+  return (
+    <EthTxParams
+      decoding = {decoding}
+    >
+    </EthTxParams>
+  );
+}
+
+// export const secondaryType = () => (
+//   <EthTxParams
+//     onClick={action('clicked')}
+//     type="secondary"
+//     disabled={boolean('disabled', false)}
+//   >
+//     {text('text', 'Click me')}
+//   </EthTxParams>
+// );
+
