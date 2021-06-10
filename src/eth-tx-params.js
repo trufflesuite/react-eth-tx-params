d0bf2a993ac2b66b7513b8ec4cd04fdb56bf50e9
Dan Finlay <dan@danfinlay.com>
2021-06-10 13:38:10 -0700

    allow scrolling overflow

--- src/eth-tx-params.js
+++ src/eth-tx-params.js
@@ -15,7 +15,7 @@ const EthTxParams = ({
       const { arguments: args, abi: { name } } = decoding;
       return (
         <div className="eth-tx-params">
-          <h2>{ name }</h2>
+          <h2>{ deCamelCase(name).toUpperCase() }</h2>
           <ol>
             { args.map((argument, index) => {
               return renderNamedItem(argument?.name, argument.value, index)
@@ -34,7 +34,7 @@ function renderNamedItem (name, item, index) {
 
   if (item.type.typeClass === 'struct') {
     return (<details key={index} open>
-      <summary>{name}</summary>
+      <summary>{deCamelCase(name)}</summary>
       <ol>
         {
           item.value.map(({ name, value: item }, index) => {
@@ -48,7 +48,7 @@ function renderNamedItem (name, item, index) {
   }
 
   return (<div key={index} className="solidity-named-item solidity-item">
-    <span className='param-name'>{ name }</span>
+    <span className='param-name'>{ deCamelCase(name) }</span>
     { renderItem(item) }
   </div>)
 }
@@ -72,27 +72,32 @@ function renderItem(item) {
       switch (item.type.typeClass) {
 
         case 'uint':
-          return (<span className="solidity-uint">
+          return (<span className="sol-item solidity-uint">
             {item.value.asBN.toString()}
           </span>)
 
         case 'bytes':
-          return (<span className="solidity-bytes">
+          return (<span className="sol-item solidity-bytes">
             {item.value.asHex}
           </span>)
 
         case 'address':
-          return (<span className="solidity-address">
+          return (<span className="sol-item solidity-address">
             <Jazzicon address={item.value.asAddress}/>
+            {item.value.asAddress}
           </span>) 
 
         default:
           console.log('item: %o', item)
-          return (<pre className="solidity-raw">
+          return (<pre className="sol-item solidity-raw">
             { inspect(new Codec.Format.Utils.Inspect.ResultInspector(item)) }
           </pre>)
       }
   }
+}
+
+function deCamelCase (label) {
+  return label.replace(/([A-Z])/g, ",$1").toLowerCase().split(',').join(' ');
 }
 
 EthTxParams.propTypes = {
