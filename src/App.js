29d49a1e2d95419b9b0ad84e6e193c9ef3ba2c0d
Dan Finlay <dan@danfinlay.com>
2021-06-10 13:25:42 -0700

    Rendering the data types that I've encountered so far

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import EthTxParams from './eth-tx-params';
+import decodings from './decodings';
+import * as Codec from '@truffle/codec';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { template: 0 };
+  }
+
+  render() {
+    const { template } = this.state;
+    const decoding = decodings[template];
+    const data = deserializeCalldataDecoding(decoding);
+    console.log('Decoding: %o', data);
+ 
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Tx Param Component</h1>
+          <div>
+            {
+              decodings.map((decoding, i) => {
+                return (
+                  <button key={i} onClick={() => {
+                    console.log('trying to click', i);
+                    this.setState({ template: i });
+                  }}>{decoding.desc}</button>
+                )  
+              })
+            }
+          </div>
+        </header>
+
+        <main>
+
+          <EthTxParams 
+            decoding={data}
+          ></EthTxParams>
+
+        </main>
+      </div>
+    );
+  }
 }
 
+function deserializeCalldataDecoding(decoding) {
+  console.log('Deserializing decoding: %o', decoding);
+  switch (decoding.kind) {
+    case "function": {
+      return {
+        ...decoding,
+        class: Codec.Format.Utils.Serial.deserializeType(decoding.class),
+        arguments: decoding.arguments.map(({ name, value }) => ({
+          name,
+          value: Codec.Format.Utils.Serial.deserializeResult(value)
+        }))
+      };
+    }
+    case "constructor": {
+      return {
+        ...decoding,
+        class: Codec.Format.Utils.Serial.deserializeType(decoding.class),
+        arguments: decoding.arguments.map(({ name, value }) => ({
+          name,
+          value: Codec.Format.Utils.Serial.deserializeResult(value)
+        }))
+      };
+    }
+    case "message": {
+      return {
+        ...decoding,
+        class: Codec.Format.Utils.Serial.deserializeType(decoding.class)
+      };
+    }
+    case "unknown": {
+      return decoding;
+    }
+    case "create": {
+      return decoding;
+    }
+  }
+}
+  
 export default App;
--- src/eth-tx-params.js
+++ src/eth-tx-params.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import * as codec from '@truffle/codec';
+import * as Codec from '@truffle/codec';
+import inspect from 'browser-util-inspect';
+import './eth-tx-params.css';
+import { Jazzicon } from '@ukstv/jazzicon-react';
 
 const EthTxParams = ({
   decoding,
 }) => {
-   
+  console.dir(decoding); 
   switch (decoding.kind) {
     case 'function':
 
       const { arguments: args, abi: { name } } = decoding;
       return (
-        <div>
+        <div className="eth-tx-params">
           <h2>{ name }</h2>
           <ol>
             { args.map((argument, index) => {
-              const { name, value } = argument;
-              return (
-                <div key={index}>
-                  <span>{name}</span>
-                  <span>{ codec.Format.Utils.Inspect.nativize(value) }</span>
-                </div>
-              );
+              return renderNamedItem(argument?.name, argument.value, index)
             })}
           </ol>
         </div>
@@ -30,10 +27,73 @@ const EthTxParams = ({
     case 'constructor':
     default:
       return 'Unable to render function data';
+  }
+};
 
+function renderNamedItem (name, item, index) {
+
+  if (item.type.typeClass === 'struct') {
+    return (<details key={index} open>
+      <summary>{name}</summary>
+      <ol>
+        {
+          item.value.map(({ name, value: item }, index) => {
+            return <li>
+              {renderNamedItem(name, item, index)}
+            </li>
+          })
+        }
+      </ol>      
+    </details>);
   }
 
-};
+  return (<div key={index} className="solidity-named-item solidity-item">
+    <span className='param-name'>{ name }</span>
+    { renderItem(item) }
+  </div>)
+}
+
+function renderUnnamedItem (item, index) {
+  return (<div key={index} className="solidity-unnamed-item solidity-item">
+    { renderItem(item) }
+  </div>)
+}
+
+// Result can be a value or an error
+// Result { type:SolidityVariableType , kind: ResultKindString }
+function renderItem(item) {
+  // Two discriminators: value or error
+  switch (item.kind) {
+    case "error":
+      return "Malformed data";
+
+    default:
+
+      switch (item.type.typeClass) {
+
+        case 'uint':
+          return (<span className="solidity-uint">
+            {item.value.asBN.toString()}
+          </span>)
+
+        case 'bytes':
+          return (<span className="solidity-bytes">
+            {item.value.asHex}
+          </span>)
+
+        case 'address':
+          return (<span className="solidity-address">
+            <Jazzicon address={item.value.asAddress}/>
+          </span>) 
+
+        default:
+          console.log('item: %o', item)
+          return (<pre className="solidity-raw">
+            { inspect(new Codec.Format.Utils.Inspect.ResultInspector(item)) }
+          </pre>)
+      }
+  }
+}
 
 EthTxParams.propTypes = {
   name: PropTypes.string,
@@ -44,5 +104,5 @@ EthTxParams.propTypes = {
   children: PropTypes.node,
   icon: PropTypes.node,
 };
-
+           
 export default EthTxParams;
